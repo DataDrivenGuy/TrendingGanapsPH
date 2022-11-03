28c8676915935de826e487c80cac604921356147
DataDrivenGuy <58902591+DataDrivenGuy@users.noreply.github.com>
2022-11-03 19:38:37 +0800

    initial commit

--- AnoGanap.py
+++ AnoGanap.py
@@ -5,5 +5,34 @@ import os
 import json
 import sys
 import geocoder
+import requests
+import os
+import json
+import pickle
 
-print('test')
+from dotenv import load_dotenv
+
+load_dotenv()
+bearer_token = os.environ['BEARER_TOKEN']
+
+client = tweepy.Client(bearer_token=bearer_token, wait_on_rate_limit=True)
+query = 'pasend'
+
+tweets = tweepy.Paginator(client.search_recent_tweets, query=query, tweet_fields=[
+                          'context_annotations', 'created_at'], max_results=100).flatten(limit=10000)
+
+# Convert the search results into a pandas dataframe
+df = pd.DataFrame(tweets)
+# Print first five the tweet ids and tweets
+df[['id', 'text']].head()
+
+with open('pasend.pickle', 'wb') as handle:
+    pickle.dump(df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# with open('pasend.pickle', 'rb') as handle:
+#     df = pickle.load(handle)
+#
+# pd.set_option('display.width', 1000)
+# pd.set_option('display.max_columns', 1000)
+# print(df)
+
--- utilities/twitter.py
+++ utilities/twitter.py
@@ -1,19 +1,14 @@
 import os
 import tweepy
 
-
 def tw_oauth():
-    # consumer_key = config('consumerKey')
-    # consumer_secret = config('consumerSecret')
-    # access_token = config('accessToken')
-    # access_token_secret = config('accessTokenSecret')
-
     consumer_key = os.environ['CONSUMER_KEY']
     consumer_secret = os.environ['CONSUMER_SECRET']
     access_token = os.environ['ACCESS_TOKEN']
     access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+    bearer_token = os.environ['BEARER_TOKEN']
 
-    auth = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    client = tweepy.Client(consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token,
+                           access_token_secret=access_token_secret)
 
-    return tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    return client
